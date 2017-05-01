39af0a881ed7cc8848c35cc559c45987556649fd
Mounir Ybanez <rinuom91@gmail.com>
2017-05-01 13:57:24 +0800

    Initial commit.

---
+++ Pyrobase/PyroAuth.swift
@@ -0,0 +1,11 @@
+//
+//  PyroAuth.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 01/05/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+class PyroAuth {
+    
+}
---
+++ Pyrobase/Pyrobase.h
@@ -0,0 +1,19 @@
+//
+//  Pyrobase.h
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 01/05/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pyrobase.
+FOUNDATION_EXPORT double PyrobaseVersionNumber;
+
+//! Project version string for Pyrobase.
+FOUNDATION_EXPORT const unsigned char PyrobaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pyrobase/PublicHeader.h>
+
+
