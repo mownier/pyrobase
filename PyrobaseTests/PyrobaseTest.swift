5561427ebab72dca24cde6e8fce2265c9eb94c39
Mounir Ybanez <rinuom91@gmail.com>
2017-05-02 16:14:57 +0800

    Added Pyrobase test.

---
+++ PyrobaseTests/PyrobaseTest.swift
@@ -0,0 +1,60 @@
+//
+//  PyrobaseTest.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 01/05/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import XCTest
+@testable import Pyrobase
+
+class PyrobaseTest: XCTestCase {
+    
+    func testInitialization() {
+        let baseURL = "https://foo.firebaseio.com"
+        let accessToken = "accessToken"
+        let requestPath = RequestPath(baseURL: baseURL, accessToken: accessToken)
+        let request = Request()
+        let pyrobase = Pyrobase(baseURL: baseURL, accessToken: accessToken, requestPath: requestPath, request: request)
+        XCTAssertNotNil(pyrobase.baseURL)
+        XCTAssertNotNil(pyrobase.accessToken)
+        XCTAssertNotNil(pyrobase.requestPath)
+        XCTAssertNotNil(pyrobase.request)
+        XCTAssertEqual(pyrobase.baseURL, baseURL)
+        XCTAssertEqual(pyrobase.accessToken, accessToken)
+    }
+    
+    func testCreate() {
+        let baseURL = "https://foo.firebaseio.com"
+        let accessToken = "accessToken"
+        let pyrobase = Pyrobase.create(baseURL: baseURL, accessToken: accessToken)
+        XCTAssertNotNil(pyrobase.baseURL)
+        XCTAssertNotNil(pyrobase.accessToken)
+        XCTAssertNotNil(pyrobase.requestPath)
+        XCTAssertEqual(pyrobase.baseURL, baseURL)
+        XCTAssertEqual(pyrobase.accessToken, accessToken)
+        XCTAssertTrue(pyrobase.requestPath is RequestPath)
+    }
+    
+    func testGet() {
+        let baseURL = "https://foo.firebaseio.com"
+        let accessToken = "accessToken"
+        let requestPath = RequestPath(baseURL: baseURL, accessToken: accessToken)
+        let request = RequestMock()
+        let pyrobase = Pyrobase(baseURL: baseURL, accessToken: accessToken, requestPath: requestPath, request: request)
+        
+        let expectation1 = expectation(description: "testGet")
+        pyrobase.get(path: "name") { value in
+            XCTAssertTrue(value is String)
+            XCTAssertEqual(value as! String, "Luche")
+            expectation1.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testPut() {
+        
+    }
+}
