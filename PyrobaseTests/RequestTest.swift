ef44dcc56335dc028237078b27cde8449d13bb19
Mounir Ybanez <rinuom91@gmail.com>
2017-05-02 16:15:10 +0800

    Added Request test.

---
+++ PyrobaseTests/RequestTest.swift
@@ -0,0 +1,53 @@
+//
+//  RequestTest.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 02/05/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import XCTest
+@testable import Pyrobase
+
+class RequestTest: XCTestCase {
+    
+    func testRead() {
+        let session = URLSessionMock()
+        let request = Request(session: session)
+        let expectation1 = expectation(description: "testRead")
+        request.read(path: "https://foo.firebaseio.com/users/12345/name.json?access_token=accessToken") { value in
+            XCTAssertTrue(value is String)
+            XCTAssertEqual(value as! String, "Luche")
+            expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testReadWithInt() {
+        let session = URLSessionMock()
+        let request = Request(session: session)
+        let expectation1 = expectation(description: "testRead")
+        request.read(path: "https://foo.firebaseio.com/users/12345/int.json?access_token=accessToken") { value in
+            XCTAssertTrue(value is String)
+            let number = Int(value as! String)
+            XCTAssertNotNil(number)
+            XCTAssertEqual(number, 101)
+            expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testReadWithDouble() {
+        let session = URLSessionMock()
+        let request = Request(session: session)
+        let expectation1 = expectation(description: "testRead")
+        request.read(path: "https://foo.firebaseio.com/users/12345/double.json?access_token=accessToken") { value in
+            XCTAssertTrue(value is String)
+            let number = Double(value as! String)
+            XCTAssertNotNil(number)
+            XCTAssertEqual(number, 101.12345)
+            expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+}
