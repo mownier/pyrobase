e1859edc8a21b00e2990ca57d28b38ce58b1b8eb
Mounir Ybanez <rinuom91@gmail.com>
2017-06-07 11:04:25 +0800

    Added RequestMethod test.

---
+++ PyrobaseTests/RequestMethodTest.swift
@@ -0,0 +1,27 @@
+//
+//  RequestMethodTest.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 07/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import XCTest
+@testable import Pyrobase
+
+class RequestMethodTest: XCTestCase {
+    
+    func testPrintedDescription() {
+        var method: RequestMethod = .get
+        XCTAssertEqual("\(method)", "GET")
+        
+        method = .post
+        XCTAssertEqual("\(method)", "POST")
+        
+        method = .put
+        XCTAssertEqual("\(method)", "PUT")
+        
+        method = .patch
+        XCTAssertEqual("\(method)", "PATCH")
+    }
+}
