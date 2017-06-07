0bc1bb6a8b8201de36af1e034bfdb1f4e988d58e
Mounir Ybanez <rinuom91@gmail.com>
2017-06-07 10:47:58 +0800

    Checkpoint: Added Authentication class.

---
+++ Pyrobase/Authentication.swift
@@ -0,0 +1,11 @@
+//
+//  Authentication.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 05/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+public class Authentication: AnyObject {
+
+}
