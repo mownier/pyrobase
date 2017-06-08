64fa3a85e13c1b2f3f26160f21c95c299746def5
Mounir Ybanez <rinuom91@gmail.com>
2017-06-08 14:58:20 +0800

    Added tests on put, post, and patch request methods.

--- PyrobaseTests/PyrobaseTest.swift
+++ PyrobaseTests/PyrobaseTest.swift
@@ -55,6 +55,101 @@ class PyrobaseTest: XCTestCase {
     }
     
     func testPut() {
+        let baseURL = "https://foo.firebaseio.com"
+        let accessToken = "accessToken"
+        let path = RequestPath(baseURL: baseURL, accessToken: accessToken)
+        let request = RequestMock()
+        let pyrobase = Pyrobase(request: request, path: path)
+        let expectedValue = ["last_name": "Valdez"]
+        let expectation1 = expectation(description: "testPut")
         
+        pyrobase.put(path: "name", value: expectedValue) { result in
+            switch result {
+            case .failed:
+                XCTFail()
+                
+            case .succeeded(let data):
+                XCTAssertTrue(data is [String: String])
+                let resultInfo = data as! [String: String]
+                XCTAssertEqual(resultInfo.count, expectedValue.count)
+                XCTAssertEqual(resultInfo["last_name"], expectedValue["last_name"])
+                
+                let absolutePath = path.build("name")
+                let content = request.content[absolutePath]
+                XCTAssertTrue(content is [String: String])
+                let contentInfo = content as! [String: String]
+                XCTAssertEqual(contentInfo.count, expectedValue.count)
+                XCTAssertEqual(contentInfo["last_name"], expectedValue["last_name"])
+            }
+            expectation1.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testPost() {
+        let baseURL = "https://foo.firebaseio.com"
+        let accessToken = "accessToken"
+        let path = RequestPath(baseURL: baseURL, accessToken: accessToken)
+        let request = RequestMock()
+        let pyrobase = Pyrobase(request: request, path: path)
+        let expectedValue = ["message": "Hello world!"]
+        let expectation1 = expectation(description: "testPost")
+        
+        pyrobase.post(path: "messages", value: expectedValue) { result in
+            switch result {
+            case .failed:
+                XCTFail()
+                
+            case .succeeded(let data):
+                XCTAssertTrue(data is [String: String])
+                let resultInfo = data as! [String: String]
+                XCTAssertEqual(resultInfo.count, 1)
+                XCTAssertNotNil(resultInfo["name"])
+                
+                let content = request.content[resultInfo["name"]!]
+                XCTAssertTrue(content is [String: String])
+                let contentInfo = content as! [String: String]
+                XCTAssertEqual(contentInfo.count, expectedValue.count)
+                XCTAssertEqual(contentInfo["message"], expectedValue["message"])
+            }
+            expectation1.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testPatch() {
+        let baseURL = "https://foo.firebaseio.com"
+        let accessToken = "accessToken"
+        let path = RequestPath(baseURL: baseURL, accessToken: accessToken)
+        let request = RequestMock()
+        let pyrobase = Pyrobase(request: request, path: path)
+        let expectedValue = ["date": "June 8, 2017"]
+        let expectation1 = expectation(description: "testPost")
+        
+        pyrobase.patch(path: "messages/abcde12345qwert", value: expectedValue) { result in
+            switch result {
+            case .failed:
+                XCTFail()
+                
+            case .succeeded(let data):
+                XCTAssertTrue(data is [String: String])
+                let resultInfo = data as! [String: String]
+                XCTAssertEqual(resultInfo.count, expectedValue.count)
+                XCTAssertEqual(resultInfo["date"], expectedValue["date"])
+                
+                let absolutePath = path.build("messages/abcde12345qwert")
+                let content = request.content[absolutePath]
+                XCTAssertTrue(content is [String: String])
+                let contentInfo = content as! [String: String]
+                XCTAssertEqual(contentInfo.count, 2)
+                XCTAssertEqual(contentInfo["message"], "Hello World!")
+                XCTAssertEqual(contentInfo["date"], expectedValue["date"])
+            }
+            expectation1.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
     }
 }
