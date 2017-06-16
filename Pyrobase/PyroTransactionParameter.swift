4ef89087291a76040324af43d7cb5bc57a1279d1
Mounir Ybanez <rinuom91@gmail.com>
2017-06-16 14:45:01 +0800

    Added file for PyroTransaction parameter.

---
+++ Pyrobase/PyroTransactionParameter.swift
@@ -0,0 +1,22 @@
+//
+//  PyroTransactionParameter.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 16/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+struct Parameter {
+    
+    let parentPath: String
+    let childKey: String
+    let mutator: (Any) -> Any
+    let completion: (RequestResult) -> Void
+    
+    init(parentPath: String, childKey: String, mutator: @escaping (Any) -> Any, completion: @escaping (RequestResult) -> Void) {
+        self.parentPath = parentPath
+        self.childKey = childKey
+        self.mutator = mutator
+        self.completion = completion
+    }
+}
