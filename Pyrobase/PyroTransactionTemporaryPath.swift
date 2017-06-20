447d95d0124c0597312306b37ca0ec1a87f635e1
Mounir Ybanez <rinuom91@gmail.com>
2017-06-20 14:45:37 +0800

    Refactored temporary path.

--- Pyrobase/PyroTransactionTemporaryPath.swift
+++ Pyrobase/PyroTransactionTemporaryPath.swift
@@ -11,28 +11,48 @@ public protocol PyroTransactionTemporaryPathProtocol {
     var key: String { get }
     var expiration: UInt { get }
     
-    func isTransactionDateExpired(_ timestamp: Double) -> Bool
+    func isTransactionDateExpired(_ timestamp: Double, now: Date) -> Bool
 }
 
 public class PyroTransactionTemporaryPath: PyroTransactionTemporaryPathProtocol {
 
-    public var key: String
-    public var expiration: UInt
+    internal var elapsedTime: PyroTransactionElapsedTimeProtocol
+    
+    internal(set) public var key: String
+    internal(set) public var expiration: UInt
     
     public class func create() -> PyroTransactionTemporaryPath {
-        return PyroTransactionTemporaryPath(key: "pyrobase_transactions", expiration: 30)
+        let key: String = "pyrobase_transactions"
+        let expiration: UInt = 30
+        let elapsedTime: Calendar = .current
+        return PyroTransactionTemporaryPath(key: key, expiration: expiration, elapsedTime: elapsedTime)
     }
     
-    public init(key: String, expiration: UInt) {
+    public init(key: String, expiration: UInt, elapsedTime: PyroTransactionElapsedTimeProtocol) {
         self.key = key
         self.expiration = expiration
+        self.elapsedTime = elapsedTime
     }
     
-    public func isTransactionDateExpired(_ timestamp: Double) -> Bool {
-        let now = Date()
+    public func isTransactionDateExpired(_ timestamp: Double, now: Date) -> Bool {
         let transactionDate = Date(timeIntervalSince1970: timestamp / 1000)
-        let seconds: Int = Calendar.current.dateComponents([.second], from: transactionDate, to: now).second ?? 0
         
-        return seconds < Int(expiration)
+        guard let seconds: Int = elapsedTime.seconds(from: transactionDate, to: now) else {
+            return false
+        }
+        
+        return seconds >= Int(expiration)
+    }
+}
+
+public protocol PyroTransactionElapsedTimeProtocol {
+    
+    func seconds(from: Date, to: Date) -> Int?
+}
+
+extension Calendar: PyroTransactionElapsedTimeProtocol {
+    
+    public func seconds(from transactionDate: Date, to now: Date) -> Int? {
+        return dateComponents([.second], from: now, to: transactionDate).second
     }
 }
