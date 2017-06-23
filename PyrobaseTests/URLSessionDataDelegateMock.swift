281acc78aaa4b7b398583ded4c98bfa30d5b58c6
Mounir Ybanez <rinuom91@gmail.com>
2017-06-23 14:16:41 +0800

    Added mock for url session data delegate.

---
+++ PyrobaseTests/URLSessionDataDelegateMock.swift
@@ -0,0 +1,13 @@
+//
+//  URLSessionDataDelegateMock.swift
+//  Pyrobase
+//
+//  Created by Mounir Ybanez on 23/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import Foundation
+
+class URLSessionDataDelegateMock: NSObject, URLSessionDataDelegate {
+
+}
