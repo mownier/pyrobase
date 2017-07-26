91b360ce722b95855075feaff098acc8c74730ee
Mounir Ybanez <rinuom91@gmail.com>
2017-07-26 15:04:54 +0800

    Added data parameter and parsing of error message.

--- Pyrobase/RequestResponse.swift
+++ Pyrobase/RequestResponse.swift
@@ -8,24 +8,39 @@
 
 public protocol RequestResponseProtocol {
     
-    func isErroneous(_ response: HTTPURLResponse?) -> Error?
+    func isErroneous(_ response: HTTPURLResponse?, data: Data?) -> Error?
 }
 
 public class RequestResponse: RequestResponseProtocol {
 
-    public func isErroneous(_ response: HTTPURLResponse?) -> Error? {
+    private(set) public var serializer: JSONSerialization.Type
+    
+    public init(serializer: JSONSerialization.Type = JSONSerialization.self) {
+        self.serializer = serializer
+    }
+    
+    public func isErroneous(_ response: HTTPURLResponse?, data: Data?) -> Error? {
         guard response != nil else {
             return nil
         }
         
         switch response!.statusCode {
-        case 400: return RequestError.badRequest
-        case 401: return RequestError.unauthorized
-        case 403: return RequestError.forbidden
-        case 404: return RequestError.notFound
-        case 500: return RequestError.internalServiceError
-        case 503: return RequestError.serviceUnavailable
+        case 400: return RequestError.badRequest(errorMessage(data))
+        case 401: return RequestError.unauthorized(errorMessage(data))
+        case 403: return RequestError.forbidden(errorMessage(data))
+        case 404: return RequestError.notFound(errorMessage(data))
+        case 500: return RequestError.internalServiceError(errorMessage(data))
+        case 503: return RequestError.serviceUnavailable(errorMessage(data))
         default: return nil
         }
+    }
+    
+    func errorMessage(_ data: Data?) -> String {
+        guard data != nil else {
+            return ""
+        }
+        
+        let info = (try? serializer.jsonObject(with: data!, options: [])) as? [AnyHashable: Any]
+        return (info?["error"] as? String) ?? ""
     }
 }
